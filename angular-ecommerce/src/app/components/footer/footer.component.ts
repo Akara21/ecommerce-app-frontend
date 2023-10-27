3a488f74c0357935532a8061596eef8a9c9db9e1
Faruk Kara <faruk@MBP-von-Faruk.fritz.box>
2023-10-27 12:06:41 +0200

    med responsive

--- angular-ecommerce/src/app/components/footer/footer.component.ts
+++ angular-ecommerce/src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AuthService} from "../../shared/services/auth.service";
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  constructor(private authService: AuthService) {
+  }
 
+  isLoggedIn() {
+    return this.authService.isLoggedIn()
+  }
 }
